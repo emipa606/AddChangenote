3ce92f7d6d2cfcc5f5821053678018af690f1e42
emipa606 <emil.offermo@litet.net>
2020-07-10 22:40:16 +0200

    First publish

---
+++ Source/AddChangenote/AddChangenote.cs
@@ -0,0 +1,93 @@
+﻿using HarmonyLib;
+using Steamworks;
+using System.IO;
+using System.Text.RegularExpressions;
+using Verse;
+using Verse.Steam;
+
+namespace AddChangenote
+{
+
+    [StaticConstructorOnStartup]
+    public class AddChangenote
+    {
+        public static WorkshopUploadable currentMod { get; set; }
+
+        static AddChangenote()
+        {
+            new Harmony("Mlie.AddChangenote").PatchAll();
+        }
+    }
+
+
+    [HarmonyPatch(typeof(Workshop), "Upload")]
+    class Patch1
+    {
+        static void Prefix(ref WorkshopUploadable item)
+        {
+            AddChangenote.currentMod = item;
+        }
+    }
+
+    [HarmonyPatch(typeof(SteamUGC), "SubmitItemUpdate")]
+    class Patch2
+    {
+        static void Prefix(ref UGCUpdateHandle_t handle, ref string pchChangeNote)
+        {
+            if (!pchChangeNote.StartsWith("[Auto-generated text]") || AddChangenote.currentMod == null)
+                return;
+            var modName = AddChangenote.currentMod.GetWorkshopName();
+            var modDirectory = AddChangenote.currentMod.GetWorkshopUploadDirectory();
+            Log.Message("Trying to add changelog-message for mod " + modName + " from directory " + modDirectory.FullName);
+            var changelogFile = new FileInfo(modDirectory.FullName + "\\About\\Changelog.txt");
+            if(!changelogFile.Exists)
+            {
+                Log.Message("Could not find changelog-file for mod " + modName + " at path " + changelogFile.FullName + ", skipping modification of changenote.");
+                return;
+            }
+            var manifestFile = new FileInfo(modDirectory.FullName + "\\About\\Manifest.xml");
+            if (!manifestFile.Exists)
+            {
+                Log.Message("Could not find manifest-file for mod " + modName + " at path " + manifestFile.FullName + ", skipping modification of changenote.");
+                return;
+            }
+            string currentVersion = null;
+            Regex versionRegex = new Regex(@"<version>\d+(?:\.\d+){1,3}<\/<version>");
+            foreach (var line in File.ReadAllLines(manifestFile.FullName))
+            {
+                Match match = versionRegex.Match(line);
+                currentVersion = match.Value;
+            }
+            if (string.IsNullOrEmpty(currentVersion))
+            {
+                Log.Message("Could not find version in manifest-file for mod " + modName + ", skipping modification of changenote.");
+                return;
+            }
+
+            bool isExtracting = false;
+            string changelogMessage = null;
+            foreach (var line in File.ReadAllLines(changelogFile.FullName))
+            {
+                if (line.StartsWith("#")) 
+                    continue;
+                Match match = versionRegex.Match(line);
+                if (isExtracting)
+                {                    
+                    if (match.Success)
+                        break;
+                    changelogMessage += line;
+                    continue;
+                }
+                if (!match.Success)
+                    continue;
+                changelogMessage += line;
+            }
+            if(string.IsNullOrEmpty(changelogMessage))
+            {
+                Log.Message("Could not find version in manifest-file for mod " + modName + ", skipping modification of changenote.");
+                return;
+            }
+            pchChangeNote = changelogMessage;
+        }
+    }
+}
---
+++ Source/AddChangenote/Settings/Mod.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace ModName
+{
+    [StaticConstructorOnStartup]
+    internal class ModNameMod : Mod
+    {
+        /// <summary>
+        /// Cunstructor
+        /// </summary>
+        /// <param name="content"></param>
+        public ModNameMod(ModContentPack content) : base(content)
+        {
+            instance = this;
+        }
+
+        /// <summary>
+        /// The instance-settings for the mod
+        /// </summary>
+        internal ModNameSettings Settings
+        {
+            get
+            {
+                if (settings == null)
+                {
+                    settings = GetSettings<ModNameSettings>();
+                }
+                return settings;
+            }
+            set
+            {
+                settings = value;
+            }
+        }
+
+        /// <summary>
+        /// The title for the mod-settings
+        /// </summary>
+        /// <returns></returns>
+        public override string SettingsCategory()
+        {
+            return "ModName";
+        }
+
+        /// <summary>
+        /// The settings-window
+        /// For more info: https://rimworldwiki.com/wiki/Modding_Tutorials/ModSettings
+        /// </summary>
+        /// <param name="rect"></param>
+        public override void DoSettingsWindowContents(Rect rect)
+        {
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.Begin(rect);
+            listing_Standard.Gap();
+            listing_Standard.CheckboxLabeled("Checkbox label", ref Settings.CheckboxValue, "Checkbox tooltip");
+            listing_Standard.Label("Int value: " + Settings.IntValue, -1, "Int tooltip");
+            listing_Standard.IntAdjuster(ref Settings.IntValue, 1, 0);
+            listing_Standard.IntRange(ref Settings.IntRangeValue, 0, 100);
+            listing_Standard.Label("Float value: " + Settings.FloatValue, -1, "Float tooltip");
+            Settings.FloatValue = Widgets.HorizontalSlider(listing_Standard.GetRect(20), Settings.FloatValue, 0, 100f, false, "Float label", null, null, 1);
+            listing_Standard.End();
+            Settings.Write();
+        }
+
+        /// <summary>
+        /// The instance of the settings to be read by the mod
+        /// </summary>
+        public static ModNameMod instance;
+
+        /// <summary>
+        /// The private settings
+        /// </summary>
+        private ModNameSettings settings;
+
+    }
+}
