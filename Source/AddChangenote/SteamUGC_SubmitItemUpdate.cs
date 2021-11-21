558eb602f0719513fdf408dd9402b70af005d10b
Mlie <mlie@litet.net>
2021-11-21 19:48:37 +0100

    Checking to see if the changenote supports multiple lines This should be line number two
    And four

--- Source/AddChangenote/SteamUGC_SubmitItemUpdate.cs
+++ Source/AddChangenote/SteamUGC_SubmitItemUpdate.cs
@@ -86,6 +86,8 @@ namespace AddChangenote
                 return;
             }
 
+            changelogMessage = changelogMessage.Replace("\n", "\n\n").Trim();
+
             pchChangeNote = changelogMessage;
         }
     }
